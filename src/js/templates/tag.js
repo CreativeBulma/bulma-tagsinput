3fb7f65d80f8b6a15b305a15eea14b079837977b
Mike A. Owens <mike@filespanker.com>
2020-09-27 04:08:01 -0400

    fix(js): Escape HTML in templates
    Closes #12

--- src/js/templates/tag.js
+++ src/js/templates/tag.js
@@ -1,6 +1,8 @@
+import { escape } from "../utils/dom";
+
 export default (data) => {
-	return `<span class="tag ${data.style}" data-value="${data.value}">
-        ${data.text}
+	return `<span class="tag ${escape(data.style)}" data-value="${escape(data.value)}">
+        ${escape(data.text)}
         ${data.removable ? '<div class="delete is-small" data-tag="delete"></div>' : ''}
     </span>`;
 };
--- src/js/templates/wrapper.js
+++ src/js/templates/wrapper.js
@@ -1,9 +1,11 @@
+import { escape } from "../utils/dom";
+
 export default (data) => {
 	return `<div class="tags-input">
-        <input class="input" type="text" placeholder="${data.placeholder}">
-        <div id="${data.uuid}-list" class="dropdown-menu" role="menu">
+        <input class="input" type="text" placeholder="${escape(data.placeholder)}">
+        <div id="${escape(data.uuid)}-list" class="dropdown-menu" role="menu">
             <div class="dropdown-content">
-                <span class="dropdown-item empty-title">${data.emptyTitle}</span>
+                <span class="dropdown-item empty-title">${escape(data.emptyTitle)}</span>
             </div>
         </div>
     </div>`;
